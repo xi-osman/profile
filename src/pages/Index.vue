e6f302785c5ec12e397ea6a2cc3ded971cabb0db
Osvaldo Perez <463779+xi-osman@users.noreply.github.com>
2024-08-05 20:34:51 -0500

    time flies 2024

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -24,16 +24,17 @@
         border="2"
       />
       <p>I'm a software developer based in Chicago, IL who enjoys building web apps that make lives of others easier. I enjoy working with JavaScript frameworks and cloud platforms.</p>
-      <p>I have over 6 years of programming experience. Currently, I lead the development team at Heartland Business Systems, building infrastructure, apps and services to power dozens of ERP systems.</p>
+      <p>I have over 8 years of programming experience, during which I have led development teams and delivered robust software solutions across various industries.</p>
+      <p>Currently, I am a Full Stack Developer at Exceleras. My role involves improving their platform and modernizing the tech stack to ensure it meets contemporary standards and delivers optimal performance for our users.</p>
       <p>Here's a few technologies I've been working with recently:</p>
       <ul class="color">
         <li>Ruby</li>
         <li>Ruby on Rails</li>
-        <li>JavaScript (ES6+)</li>
+        <li>JavaScript &amp; TypeScript</li>
         <li>HTML &amp; CSS</li>
         <li>Vue</li>
         <li>Node.js</li>
-        <li>Express</li>
+        <li>Angular</li>
         <li>C#</li>
       </ul>
       <p>Outside of programming and spending time with my wife, I enjoy video games, videography, tacos and discovering new places.</p>
@@ -57,11 +58,11 @@
             </svg> GitHub
           </a>
           <a href="https://twitter.com/xiosman" target="_blank" rel="nofollow noopener noreferrer">
-            <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 612 612">
-              <title>Twitter</title>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter-x"
+              viewBox="0 0 16 16">
+              <title>X</title>
               <path
-                d="M612,116.258c-22.525,9.981-46.694,16.75-72.088,19.772c25.929-15.527,45.777-40.155,55.184-69.411 c-24.322,14.379-51.169,24.82-79.775,30.48c-22.907-24.437-55.49-39.658-91.63-39.658c-69.334,0-125.551,56.217-125.551,125.513 c0,9.828,1.109,19.427,3.251,28.606C197.065,206.32,104.556,156.337,42.641,80.386c-10.823,18.51-16.98,40.078-16.98,63.101 c0,43.559,22.181,81.993,55.835,104.479c-20.575-0.688-39.926-6.348-56.867-15.756v1.568c0,60.806,43.291,111.554,100.693,123.104 c-10.517,2.83-21.607,4.398-33.08,4.398c-8.107,0-15.947-0.803-23.634-2.333c15.985,49.907,62.336,86.199,117.253,87.194 c-42.947,33.654-97.099,53.655-155.916,53.655c-10.134,0-20.116-0.612-29.944-1.721c55.567,35.681,121.536,56.485,192.438,56.485 c230.948,0,357.188-191.291,357.188-357.188l-0.421-16.253C573.872,163.526,595.211,141.422,612,116.258z"
-              />
+                d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
             </svg> Twitter
           </a>
           <a href="https://vimeo.com/xiosman" target="_blank" rel="nofollow noopener noreferrer">
